ccba76c2ddc9bebaed1d00587cdcfa4b1c4a30b7
Wal <romerow169@gmail.com>
2025-01-10 09:14:55 -0600

    cleaning backend branch

--- src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <footer class="text-white text-center py-8 mt-4">
-
-        <p class="font-bold">
-            Francisco Escobar &copy; 2025
-        </p>
-    </footer>
-</template>
