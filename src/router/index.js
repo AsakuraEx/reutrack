678c445540c1617a16a3a82467a88f0d489da945
Fescobar28 <franescobar97@hotmail.com>
2025-01-20 15:24:33 -0600

    Agregada pagina 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:catchAll(.*)',
+      name: 'notFound',
+      component: ()=>import('../views/PageNotFound.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen flex flex-col md:flex-row gap-2 justify-center items-center">
+        <img src="../../public/images/not-found.svg" alt="" class="w-1/4">
+        <div class="block">
+            <h1 class="text-5xl font-black text-purple-500 mb-4 text-center uppercase">404 Page Not Found</h1>
+            <h2 class="font-black leading-3 text-xl text-center">La página a la que intenta acceder, no existe.</h2>
+        </div>
+    </div>
+</template>
