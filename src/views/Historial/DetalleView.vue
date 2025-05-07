464a95753d42b3f1998fce927b103487ccd52e12
Wal <romerow169@gmail.com>
2025-05-07 13:46:48 -0600

    headers

--- src/views/Historial/DetalleView.vue
+++ src/views/Historial/DetalleView.vue
@@ -69,6 +69,9 @@
         //window.location.replace(pdf.value)
         try {
             const response = await axios.get(baseURL + `/reuniones/pdf/${id}`, {
+                'Access-Control-Allow-Origin': 'https://api-reutrack.salud.gob.sv/api/',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
                 responseType: 'blob', // Permite manejar archivos binarios
             });
 
