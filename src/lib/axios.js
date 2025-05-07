838e8a3a84cf345aef087ca44623b910ff6d7a23
Wal <romerow169@gmail.com>
2025-05-07 12:18:59 -0600

    Correcciónd de headers (deben manejarse en el backend exclusivamente)

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -8,9 +8,6 @@ const router = useRouter()
 const api = axios.create({
   baseURL: baseURL,
   headers: {
-    'Access-Control-Allow-Origin': baseURL,
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Content-Type': 'application/json',
   }
 });
