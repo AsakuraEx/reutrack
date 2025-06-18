3c6f4f54a0cbf971406898cd7acbba456bb5bea9
WalkerR9 <romerow169@gmail.com>
2025-06-18 11:25:09 -0600

    app.js como principal

--- src/app.js
+++ src/app.js
@@ -75,13 +75,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//const port = process.env.BACKEND_PORT;
-//const host = process.env.BACKEND_HOST
+const port = process.env.PORT;
+const host = process.env.HOST
 
-//app.listen(port, host, () => {
-//  if(process.env.BACKEND_PORT = 3100){
-//    console.log(`Servidor escuchando en http://${host}:${port}`);
- // }
-//  });
+app.listen(port, host, () => {
+  console.log(`RUNNING SERVER`);
+});
 
 module.exports = app;
