4d4553781bb99e10a1242ecdf5de3e87f41bebc4
Fescobar28 <franescobar97@hotmail.com>
2025-07-04 09:23:18 -0600

    Ajuste de tiempo de expiración a 2 horas - solicita Arleny

--- src/views/Reuniones/ReunionView.vue
+++ src/views/Reuniones/ReunionView.vue
@@ -59,7 +59,7 @@
     const expiracionCodigo = () => {
         const fechaActual = new Date();
         const expiracion = new Date(fechaActual);
-        expiracion.setMinutes(fechaActual.getMinutes() + 60);
+        expiracion.setMinutes(fechaActual.getMinutes() + 120);
 
         // Guarda en formato ISO (UTC) o timestamp para evitar problemas de zona horaria
         formData.fecha = fechaActual.toISOString(); // Ej: "2025-05-31T20:13:00.000Z"
