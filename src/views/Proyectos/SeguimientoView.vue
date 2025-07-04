a2c8a1b89491ff7a7014a99b6876b878a106e725
Fescobar28 <franescobar97@hotmail.com>
2025-07-04 08:56:00 -0600

    Responsive realizado

--- src/views/Proyectos/SeguimientoView.vue
+++ src/views/Proyectos/SeguimientoView.vue
@@ -71,76 +71,88 @@
                 <h1 class="text-2xl text-purple-500 font-black text-center">Seguimiento del proyecto: {{ proyecto.nombre }}</h1>
                 <span class="text-lg font-light text-gray-400 text-center">Fecha de registro de proyecto en reutrack: {{ transformarFecha(proyecto.createdAt) }}</span>
             </div>
-    
+
+            <!-- Punto central -->
+            <div class="hidden lg:flex justify-center w-full">
+                <div class="w-8 h-8 bg-gray-500 rounded-full border-4 border-white z-10 mx-auto md:mx-0"></div>
+            </div>
+
             <div class="relative">
-    
+
                 <!-- Línea central de la linea cronologica -->
-                <div class="absolute left-1/2 top-0 h-full w-1 bg-purple-100 transform -translate-x-1/2"></div>
+                <div class="absolute left-1/2 top-0 h-full w-1 bg-purple-100 transform -translate-x-1/2 hidden lg:block"></div>
     
                 <!-- Items de al linea cronologica -->
                 <div
                     v-for="(item, index) in reuniones"
                     :key="index"
-                    class="w-full flex mb-8 items-center"
+                    class="flex flex-col md:flex-row lg:mb-8 items-center w-full"
                 >
                     <!-- Lado izquierdo -->
-                    <div class="w-1/2 flex justify-end pr-8 text-right">
+                    <div class="w-full md:w-1/2 md:pr-8 text-right text-white mb-2 md:mb-0 lg:flex lg:justify-end">
                         <div v-if="numeroPar(index)">
     
                             <!-- Card -->
                             <div 
-                                class="border-2 pb-4 border-gray-700 shadow-md rounded-md w-[450px] h-40 text-black bg-purple-100"
+                                class="border-2 pb-4 border-gray-700 shadow-md rounded-md w-full lg:w-[450px] h-48 text-black bg-white"
                             >
                                 <!-- Titulo de card -->
                                 <h1 
-                                    class="text-center text-xl font-bold text-white py-2"
+                                    class="text-center text-xl font-bold py-2 text-white"
                                     :class="claseEstado(item.estado.nombre)"
                                 >
                                     {{ item.nombre }}
                                 </h1>
     
                                 <!-- Contenido de card -->
-                                <div class="flex flex-col items-center gap-2 font-medium justify-center text-center py-2 h-full">
+                                <div class="relative flex flex-col items-center gap-2 font-medium justify-center text-center py-2 h-full w-full">
+                                    
+                                    <p>{{ item.version.nombre }}</p>
                                     <p>{{ item.lugar }}</p>
-                                    <p class="bg-gray-500 rounded-md px-2 py-1 text-white w-fit mx-auto mb-2 text-md">{{ item.user.nombre }}</p>
+                                    <p class="lg:hidden">{{ transformarFecha(item.createdAt) }}</p>
+                                    <p class="bg-purple-500 rounded-md px-2 py-1 text-white w-fit mx-auto mb-2 text-md">{{ item.user.nombre }}</p>
                                     <br>
                                 </div>
                             </div>
     
                         </div>
                         <div v-else>
-                            <p class="bg-purple-500 rounded-md px-4 py-2 text-white w-fit mb-2 text-startz">{{ transformarFecha(item.createdAt) }}</p>
+                            <p class="bg-purple-500 rounded-md px-4 py-2 text-white w-full lg:w-fit mb-2 hidden lg:block">{{ transformarFecha(item.createdAt) }}</p>
                         </div>
                     </div>
     
                     <!-- Punto central -->
-                    <div class="w-8 h-8 bg-purple-500 rounded-full border-4 border-white z-10"></div>
+                    <div class="w-8 h-8 bg-purple-500 rounded-full border-4 border-white z-10 mx-auto md:mx-0 hidden lg:block"></div>
     
                     <!-- Lado derecho -->
-                    <div class="w-1/2 pl-8">
+                    <div class="w-full md:w-1/2 md:pl-8 text-left text-white mt-2 md:mt-0">
                         <div v-if="numeroPar(index)">
     
-                            <p class="bg-purple-500 rounded-md px-4 py-2 text-white w-fit text-start mb-2">{{ transformarFecha(item.createdAt) }}</p>
+                            <p class="bg-purple-500 rounded-md px-4 py-2 text-white w-full lg:w-fit text-start mb-2 hidden lg:block">{{ transformarFecha(item.createdAt) }}</p>
     
                         </div>
                         <div v-else>
     
                             <!-- Card -->
                             <div 
-                                class="border-2 pb-4 border-gray-700 shadow-md rounded-md w-[450px] h-40 text-black bg-purple-100"
+                                class="border-2 pb-4 border-gray-700 shadow-md rounded-md lg:w-[450px] h-48 text-black bg-white"
                             >
                                 <!-- Titulo de card -->
                                 <h1 
-                                    class="text-center text-xl font-bold text-white py-2"
+                                    class=" flex justify-evenly items-center gap-4 text-center text-xl font-bold py-2 text-white"
                                     :class="claseEstado(item.estado.nombre)"
                                 >
                                     {{ item.nombre }}
+                                    
                                 </h1>
     
                                 <!-- Contenido de card -->
-                                <div class="flex flex-col items-center gap-2 font-medium justify-center text-center py-2 h-full">
+                                <div class="relative flex flex-col items-center gap-2 font-medium justify-center text-center py-2 h-full">
+
+                                    <p>{{ item.version.nombre }}</p>
                                     <p>{{ item.lugar }}</p>
-                                    <p class="bg-gray-500 rounded-md px-2 py-1 text-white w-fit mx-auto mb-2 text-md">{{ item.user.nombre }}</p>
+                                    <p class="lg:hidden">{{ transformarFecha(item.createdAt) }}</p>
+                                    <p class="bg-purple-500 rounded-md px-2 py-1 text-white w-fit mx-auto mb-2 text-md">{{ item.user.nombre }}</p>
                                     <br>
                                 </div>
                             </div>
@@ -149,7 +161,27 @@
                     </div>
     
                 </div>
+
+                <!-- Punto central -->
+                <div class="hidden lg:flex justify-center w-full">
+                     <div class="w-8 h-8 bg-gray-500 rounded-full border-4 border-white z-10 mx-auto md:mx-0"></div>
+                </div>
     
+            </div>
+
+            <div class="flex justify-center items-center mt-8">
+                <div class="px-6 py-1 bg-purple-200 text-purple-700">
+                    Codigo de colores
+                </div>
+                <div :class="claseEstado('Iniciado')" class="px-3 py-1">
+                    Iniciado
+                </div>
+                <div :class="claseEstado('Finalizado')" class="px-3 py-1">
+                    Finalizado
+                </div>
+                <div :class="claseEstado('Cancelado')" class="px-3 py-1">
+                    Cancelado
+                </div>
             </div>
     
         </div>
