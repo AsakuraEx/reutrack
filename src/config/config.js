67afbbd98b8710596daea8a4c55ad2e90f94c1a4
WalkerR9 <romerow169@gmail.com>
2025-07-21 10:47:02 -0600

    correccion finalizada

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     username: process.env.DEV_DB_USERNAME,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
-    host: process.env.DEV_DB_HOSTNAME,
+    host: process.env.DEV_DB_HOST,
     port: process.env.DEV_DB_PORT,
     dialect: 'mariadb',
     connectTimeout: 1000,
